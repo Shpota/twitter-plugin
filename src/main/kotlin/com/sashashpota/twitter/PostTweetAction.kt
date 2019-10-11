a7b16de385b8d291b591d4ae5419b0fc5a5e7b64
Sasha Shpota <sashashpota@gmail.com>
2019-10-11 21:37:07 +0300

    Refactorings and simplifications

--- src/main/kotlin/com/sashashpota/twitter/PostTweetAction.kt
+++ src/main/kotlin/com/sashashpota/twitter/PostTweetAction.kt
@@ -4,7 +4,6 @@ import com.intellij.ide.BrowserUtil.browse
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR
-import org.apache.commons.lang.StringUtils.isBlank
 import org.jetbrains.annotations.NotNull
 import java.io.UnsupportedEncodingException
 import java.net.URLEncoder.encode
@@ -20,15 +19,9 @@ class PostTweetAction : AnAction() {
     }
 
     override fun update(@NotNull event: AnActionEvent) {
-        val selectedTextBlank = isBlank(selectedText(event))
-        event.presentation.isVisible = !selectedTextBlank
+        event.presentation.isVisible = !selectedText(event).isNullOrBlank()
     }
 
-    private fun selectedText(event: AnActionEvent): String? {
-        val editor = event.getData(EDITOR)
-        if (editor != null) {
-            return editor.caretModel.currentCaret.selectedText
-        }
-        return null
-    }
+    private fun selectedText(event: AnActionEvent) =
+            event.getData(EDITOR)?.caretModel?.currentCaret?.selectedText
 }
